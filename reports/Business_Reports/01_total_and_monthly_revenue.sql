b0cb1ec655581040f9c105d16e46efbd970d2e39
M. Hruday Bhaskar <hrudaybhaskar45@gmail.com>
2026-01-03 03:02:52 +0530

    Update 01_total_and_monthly_revenue.sql

--- reports/Business_Reports/01_total_and_monthly_revenue.sql
+++ reports/Business_Reports/01_total_and_monthly_revenue.sql
@@ -4,7 +4,7 @@ Layer: Gold / Fact Table
 Goal: 
    1. Calculate total revenue and total number of orders (Grand Total)
    2. Calculate monthly revenue trend
- Source Tables: gold.fact_sales, gold.dim_date
+Source Tables: gold.fact_sales, gold.dim_date
 Created: 2026-01-03
 ***********************************************************************************/
 
