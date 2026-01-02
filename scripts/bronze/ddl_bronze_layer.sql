5dc5b315e86ecc9030521cc2b5af6e4833785ec9
M. Hruday Bhaskar <hrudaybhaskar45@gmail.com>
2026-01-02 21:32:35 +0530

    Update ddl_bronze_layer.sql

--- scripts/bronze/ddl_bronze_layer.sql
+++ scripts/bronze/ddl_bronze_layer.sql
@@ -12,11 +12,11 @@ IF OBJECT_ID ('bronze.olist_customers_dataset' , 'U') IS NOT NULL
 	DROP TABLE bronze.olist_customers_dataset;
 
 CREATE TABLE bronze.olist_customers_dataset (
-	customer_id					NVARCHAR(MAX),
-	customer_unique_id			NVARCHAR(MAX),
-	customer_zip_code_prefix	NVARCHAR(MAX),
-	customer_city				NVARCHAR(MAX),
-	customer_state				NVARCHAR(MAX)
+	customer_id					NVARCHAR(50),
+	customer_unique_id			NVARCHAR(50),
+	customer_zip_code_prefix	INT,
+	customer_city				NVARCHAR(50),
+	customer_state				NVARCHAR(2)
 );
 
 
@@ -24,11 +24,11 @@ IF OBJECT_ID ('bronze.olist_geolocation_dataset' , 'U') IS NOT NULL
 	DROP TABLE bronze.olist_geolocation_dataset;
 
 CREATE TABLE bronze.olist_geolocation_dataset (
-	geolocation_zip_code_prefix NVARCHAR(MAX),
-	geolocation_lat				NVARCHAR(MAX),
-	geolocation_lng				NVARCHAR(MAX),
-	geolocation_city			NVARCHAR(MAX),
-	geolocation_state			NVARCHAR(MAX)
+	geolocation_zip_code_prefix INT,
+	geolocation_lat				DECIMAL(30,20),
+	geolocation_lng				DECIMAL(30,20),
+	geolocation_city			NVARCHAR(50),
+	geolocation_state			NVARCHAR(2)
 );
 
 
@@ -36,13 +36,13 @@ IF OBJECT_ID ('bronze.olist_order_items_dataset' , 'U') IS NOT NULL
 	DROP TABLE bronze.olist_order_items_dataset ;
 
 CREATE TABLE bronze.olist_order_items_dataset (
-	order_id			NVARCHAR(MAX),
-	order_item_id		NVARCHAR(MAX),
-	product_id			NVARCHAR(MAX),
-	seller_id			NVARCHAR(MAX),
-	shipping_limit_date NVARCHAR(MAX),
-	price				NVARCHAR(MAX),
-	freight_value		NVARCHAR(MAX)
+	order_id			NVARCHAR(50),
+	order_item_id		INT,
+	product_id			NVARCHAR(50),
+	seller_id			NVARCHAR(50),
+	shipping_limit_date DATETIME2(0),
+	price				DECIMAL(10,2),
+	freight_value		DECIMAL(10,2)
 );
 
 
@@ -50,11 +50,11 @@ IF OBJECT_ID ('bronze.olist_order_payments_dataset' , 'U') IS NOT NULL
 	DROP TABLE bronze.olist_order_payments_dataset;
 
 CREATE TABLE bronze.olist_order_payments_dataset (
-	order_id				NVARCHAR(MAX),
-	payment_sequential		NVARCHAR(MAX),
-	payment_type			NVARCHAR(MAX),
-	payment_installments	NVARCHAR(MAX),
-	payment_value			NVARCHAR(MAX)
+	order_id				NVARCHAR(50),
+	payment_sequential		INT,
+	payment_type			NVARCHAR(20),
+	payment_installments	INT,
+	payment_value			DECIMAL(10,2)
 );
 
 
@@ -62,13 +62,13 @@ IF OBJECT_ID ('bronze.olist_order_reviews_dataset' , 'U') IS NOT NULL
 	DROP TABLE bronze.olist_order_reviews_dataset;
 
 CREATE TABLE bronze.olist_order_reviews_dataset (
-	review_id				NVARCHAR(MAX),
-	order_id				NVARCHAR(MAX),
-	review_score			NVARCHAR(MAX),
+	review_id				NVARCHAR(50),
+	order_id				NVARCHAR(50),
+	review_score			INT,
 	review_comment_title	NVARCHAR(MAX),
 	review_comment_message	NVARCHAR(MAX),
-	review_creation_date	NVARCHAR(MAX),
-	review_answer_timestamp NVARCHAR(MAX)
+	review_creation_date	DATETIME2(0),
+	review_answer_timestamp DATETIME2(0)
 );
 
 
@@ -76,14 +76,14 @@ IF OBJECT_ID ('bronze.olist_orders_dataset' , 'U') IS NOT NULL
 	DROP TABLE bronze.olist_orders_dataset;
 
 CREATE TABLE bronze.olist_orders_dataset (
-	order_id						NVARCHAR(MAX),
-	customer_id						NVARCHAR(MAX),
-	order_status					NVARCHAR(MAX),
-	order_purchase_timestamp		NVARCHAR(MAX),
-	order_approved_at				NVARCHAR(MAX),
-	order_delivered_carrier_date	NVARCHAR(MAX),
-	order_delivered_customer_date	NVARCHAR(MAX),
-	order_estimated_delivery_date	NVARCHAR(MAX)
+	order_id						NVARCHAR(50),
+	customer_id						NVARCHAR(50),
+	order_status					NVARCHAR(50),
+	order_purchase_timestamp		DATETIME2(0),
+	order_approved_at				DATETIME2(0),
+	order_delivered_carrier_date	DATETIME2(0),
+	order_delivered_customer_date	DATETIME2(0),
+	order_estimated_delivery_date	DATETIME2(0)
 );
 
 
@@ -91,15 +91,15 @@ IF OBJECT_ID ('bronze.olist_products_dataset' , 'U') IS NOT NULL
 	DROP TABLE bronze.olist_products_dataset;
 
 CREATE TABLE bronze.olist_products_dataset (
-	product_id					NVARCHAR(MAX),
-	product_category_name		NVARCHAR(MAX),
-	product_name_lenght			NVARCHAR(MAX),
-	product_description_lenght	NVARCHAR(MAX),
-	product_photos_qty			NVARCHAR(MAX),
-	product_weight_g			NVARCHAR(MAX),
-	product_length_cm			NVARCHAR(MAX),
-	product_height_cm			NVARCHAR(MAX),
-	product_width_cm			NVARCHAR(MAX)
+	product_id					NVARCHAR(50),
+	product_category_name		NVARCHAR(50),
+	product_name_lenght			INT,
+	product_description_lenght	INT,
+	product_photos_qty			INT,
+	product_weight_g			INT,
+	product_length_cm			INT,
+	product_height_cm			INT,
+	product_width_cm			INT
 );
 
 
@@ -107,10 +107,10 @@ IF OBJECT_ID ('bronze.olist_sellers_dataset' , 'U') IS NOT NULL
 	DROP TABLE bronze.olist_sellers_dataset;
 
 CREATE TABLE bronze.olist_sellers_dataset (
-	seller_id				NVARCHAR(MAX),
-	seller_zip_code_prefix	NVARCHAR(MAX),
-	seller_city				NVARCHAR(MAX),
-	seller_state			NVARCHAR(MAX)
+	seller_id				NVARCHAR(50),
+	seller_zip_code_prefix	INT,
+	seller_city				NVARCHAR(50),
+	seller_state			NVARCHAR(2)
 );
 
 
@@ -118,6 +118,6 @@ IF OBJECT_ID ('bronze.product_category_name_translation' , 'U') IS NOT NULL
 	DROP TABLE bronze.product_category_name_translation;
 
 CREATE TABLE bronze.product_category_name_translation (
-	product_category_name			NVARCHAR(MAX),
-	product_category_name_english	NVARCHAR(MAX)
+	product_category_name			NVARCHAR(50),
+	product_category_name_english	NVARCHAR(50)
 );
