1b57ae24d71ed419c95f906b9748fdc6dd1fd1df
M. Hruday Bhaskar <hrudaybhaskar45@gmail.com>
2026-01-03 14:33:54 +0530

    Create 02_product_categories_with_highest_revenue.sql

---
+++ reports/Business_Reports/02_product_categories_with_highest_revenue.sql
@@ -0,0 +1,15 @@
+/********************************************************************************
+Report: Product categories that contributed the highest revenue and profit.
+Source Tables: gold.fact_sales, gold.fact_products
+Created: 2026-01-03
+***********************************************************************************/
+
+SELECT 
+    p.product_category_name,
+    COUNT(DISTINCT f.order_key) AS total_orders,
+    SUM(f.total_product_value) AS total_revenue,
+    SUM(f.total_product_value) / COUNT(DISTINCT f.order_key) AS avg_order_value
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p ON f.product_key = p.product_key
+GROUP BY p.product_category_name
+ORDER BY total_revenue DESC;
