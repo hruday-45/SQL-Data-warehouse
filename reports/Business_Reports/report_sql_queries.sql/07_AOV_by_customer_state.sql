1c4ff3ecc683c3f5780ea46e4abccd11a0637b6c
M. Hruday Bhaskar <hrudaybhaskar45@gmail.com>
2026-01-03 15:43:13 +0530

    Create 07_AOV_by_customer_state.sql

---
+++ reports/Business_Reports/report_sql_queries.sql/07_AOV_by_customer_state.sql
@@ -0,0 +1,15 @@
+/********************************************************************************
+Report: AOV by customer state
+Source Views: gold.fact_sales, gold.dim_customers
+Created: 2026-01-03
+***********************************************************************************/
+
+SELECT 
+    c.customer_state,
+    COUNT(DISTINCT f.order_key) AS total_orders,
+    SUM(f.total_product_value) AS total_revenue,
+    SUM(f.total_product_value) / COUNT(DISTINCT f.order_key) AS average_order_value
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c ON f.customer_key = c.customer_key
+GROUP BY c.customer_state
+ORDER BY average_order_value DESC;
